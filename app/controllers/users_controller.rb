8a59bc0182e926c8ff19a61518d7ad302c00e246
141naoto <naot.ishii141@gmail.com>
2018-09-21 14:44:33 +0900

    resubmit Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only:[:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   	@books = @user.books
@@ -23,6 +25,13 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
     @book = Book.new
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   private
